4e5814858805158927948d06b32fc95c44710d7f
Justin Hoang <me@justinhoang.ca>
2015-08-28 15:18:44 -0400

    implement create reminder logic

--- public/js/controllers/ReminderController.js
+++ public/js/controllers/ReminderController.js
@@ -1,20 +1,33 @@
 angular.module('ReminderModule', [])
   .controller('ReminderController', function($scope, $interval, ReminderFactory) {
     $scope.init = function(){
-      ReminderFactory.getAll().then(function(reminders){
-        $scope.getReminders();
-        $interval($scope.getReminders, 2500);
-      });
+      $scope.clearCreateModal();
+      $scope.getReminders();
+      $interval($scope.getReminders, 2500);
     };
+
     $scope.getReminders = function(){
         ReminderFactory.getAll().then(function(reminders){
             $scope.reminders = reminders.data;
         });
     };
+
     $scope.deleteAllReminders = function(){
         ReminderFactory.deleteAll().then(function(reminders){
             $scope.reminders = [];
         });
     };
+
+    $scope.clearCreateModal = function(){
+      $scope.data = {};
+    };
+
+    $scope.createNew = function() {
+        ReminderFactory.create(angular.copy($scope.data)).then(function(){
+          $scope.clearCreateModal();
+          $scope.getReminders();
+        });
+    };
+
     $scope.init();
   });

0e1734378b520d9a27614d6cadea75375b5fe671
Justin Hoang <me@justinhoang.ca>
2015-08-27 23:25:36 -0400

    add create and delete reminder buttons on ui

--- public/js/controllers/ReminderController.js
+++ public/js/controllers/ReminderController.js
@@ -1,9 +1,20 @@
 angular.module('ReminderModule', [])
   .controller('ReminderController', function($scope, $interval, ReminderFactory) {
+    $scope.init = function(){
+      ReminderFactory.getAll().then(function(reminders){
+        $scope.getReminders();
+        $interval($scope.getReminders, 2500);
+      });
+    };
     $scope.getReminders = function(){
         ReminderFactory.getAll().then(function(reminders){
             $scope.reminders = reminders.data;
         });
     };
-    $interval($scope.getReminders, 2500);
+    $scope.deleteAllReminders = function(){
+        ReminderFactory.deleteAll().then(function(reminders){
+            $scope.reminders = [];
+        });
+    };
+    $scope.init();
   });
